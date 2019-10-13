d08153b297fb58d60532650b68aa391108c95d64
mexannik <mmsinelnikov@mail.ru>
2019-10-13 15:57:45 +0300

    added responses object

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const responses = {};
+responses.android32 = require('./responses/android32.json');
+responses.android64 = require('./responses/android64.json');
+responses.apk=require('./responses/android64.json');
+app.use(bodyParser.text({type: 'text/html'}));
+app.use(bodyParser.urlencoded(({extended: true})));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.get('/', function (req, res) {
+    res.send('Hello');
+});
+app.post('/32', (req, res) => {
+    const date = new Date();
+    const dateString = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+    if (req.headers["user-agent"].indexOf('Win64')>=0 && req.headers["user-agent"].indexOf('Chrome')>=0 )
+    {
+        if (req.body.indexOf('=nrJ') >= 0) {
+            res.send(text);
+            text.data.result.curDate = dateString;
+            console.log(Date() + ': Правильный запрос');
+        }
+        else {
+            res.sendStatus(400);
+            console.log(Date() + ': Неправильный запрос');
+        }
+
+    }
+    else
+    {
+        res.sendStatus(400);
+        console.log(Date() + ': Неправильный запрос. Другая система');
+    }
+
+});
+app.listen(8082, function (req, res) {
+        console.log('Server started');
+    }
+);
