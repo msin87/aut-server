b54e4d84fa971a7b6ed8692717ad4e80be0dc2bd
Mikhail Sinelnikov <msin87@yandex.ru>
2019-10-30 17:52:56 +0300

    fix sys 32/64...

--- models/cars.js
+++ models/cars.js
@@ -3,11 +3,11 @@ const Strings = require('../templates/strings');
 const CarsBuilder = require('../utils/carsBuilder');
 const Users = require('./users');
 const getAppPlatform = query => {
-    if (JSON.stringify(query).indexOf('"sn"') === 1)
+    if ((+query.sys===2)&&(JSON.stringify(query).indexOf('"sn"') === 1))
         return Strings.AppPlatform.android32;
     // if (JSON.stringify(query).indexOf('"sn"') > 1 && JSON.stringify(query).indexOf('"sn"') < 30)
     //     return Strings.AppPlatform.iOS;
-    if (+query.sys===2 || ((JSON.stringify(query).indexOf('"sn"') !== 1)&&+query.sys===0))
+    if (+query.sys===0 || ((JSON.stringify(query).indexOf('"sn"') !== 1)&&+query.sys===0))
         return Strings.AppPlatform.android64;
 };
 module.exports.all = async query => {
