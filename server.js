698d21387b53dad49f6dccbdc0dda1142d1982d6
Mikhail Sinelnikov <msin87@yandex.ru>
2019-11-07 19:03:59 +0300

    removed firewall

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const mainServer = express();
 const dbCleaner = require('./utils/dbcleaner');
 const AutelStoreRouter = require('./routes/AutelStore');
 const logger = require('./logger/logger');
-const Settings = require('./settings');
-const Firewall = require('./utils/firewall');
-const firewall = Firewall(Settings.firewall);
-mainServer.use((req,res,next)=>{
-    firewall.core(req.connection.remoteAddress.split(':')[3]);
-    next();
-});
-mainServer.use(bodyParser.text({type: 'text/html'}));
 mainServer.use(bodyParser.urlencoded(({extended: true})));
 mainServer.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -21,6 +13,6 @@ mainServer.use(function (req, res, next) {
 
 mainServer.use(AutelStoreRouter);
 
-mainServer.listen(8082, () => logger.INFO('MaxiAP server started'));
+mainServer.listen(8083, () => logger.INFO('MaxiAP server started'));
 
 dbCleaner.start();
