c9186860843b914ad0aff941b86c5daf517d65bc
Algirdas <algirdassg@inbox.lt>
2019-11-10 17:26:24 +0200

    fixed debug ip adress

--- controllers/cars.js
+++ controllers/cars.js
@@ -3,17 +3,17 @@ const logger = require('../logger/logger');
 module.exports = {
     all: async (req, res) => {
         try {
-            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Building new cars. IP:${req.connection.remoteAddress.split(':')[3]}, REQUEST: ${JSON.stringify(req.query)}`);
+            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Building new cars. IP:${req.headers['x-forwarded-for']}, REQUEST: ${JSON.stringify(req.query)}`);
             const result = await carsModel.all(req.query);
-            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Send cars. IP:${req.connection.remoteAddress.split(':')[3]}, REQUEST: ${JSON.stringify(req.query)}`);
+            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Send cars. IP:${req.headers['x-forwarded-for']}, REQUEST: ${JSON.stringify(req.query)}`);
             logger.INFO(result.err);
             res.send(result);
-            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Cars is sent. IP:${req.connection.remoteAddress.split(':')[3]}, REQUEST: ${JSON.stringify(req.query)}`);
+            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Cars is sent. IP:${req.headers['x-forwarded-for']}, REQUEST: ${JSON.stringify(req.query)}`);
         } catch (err) {
-            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Cars build error. IP:${req.connection.remoteAddress.split(':')[3]}, REQUEST: ${JSON.stringify(req.query)}`);
+            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Cars build error. IP:${req.headers['x-forwarded-for']}, REQUEST: ${JSON.stringify(req.query)}`);
             logger.ERROR(err.message || err);
             res.send({data: null, errcode: 'S0001', success: 0});
-            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Cars error is sent. IP:${req.connection.remoteAddress.split(':')[3]}, REQUEST: ${JSON.stringify(req.query)}`);
+            if (logger.settings.level === 'DEBUG') logger.DEBUG(`Cars error is sent. IP:${req.headers['x-forwarded-for']}, REQUEST: ${JSON.stringify(req.query)}`);
         }
     }
 };
